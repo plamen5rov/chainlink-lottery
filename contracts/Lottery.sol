e8c1b58132ec6db510de5b894d2d86e82397c114
Plamen <biznetmen@gmail.com>
2022-09-20 10:53:06 +0300

    Lottery contract created

---
+++ contracts/Lottery.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+contract Lottery {
+    using SafeMathChainlink for uint256;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    uint256 usdEntryFee;
+
+    constructor(address _ethUsdPriceFeed) public {
+        ethUsdPriceFeed = AggregatorV3Interface(_ethUsdPriceFeed);
+        usdEntryFee = 50;
+    }
+
+    function enter() public payable {
+        require(msg.value >= getEntranceFee(), "Not enough ETH to enter!");
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        uint256 precision = 1 * 10**18;
+        uint256 price = getLatestEthUsdPrice(); // 8 decimals
+        uint256 costToEnter = (precision / price) * (usdEntryFee * 10000000);
+        return costToEnter;
+    }
+
+    function getLatestEthUsdPrice() public view returns (uint256) {
+        (
+            ,
+            /*uint80 roundID*/
+            int256 price,
+            ,
+            ,
+
+        ) = /*uint256 startedAt*/
+            /*uint256 timeStamp*/
+            /*uint80 answeredInRound*/
+            ethUsdPriceFeed.latestRoundData();
+        return uint256(price);
+    }
+
+    // function startLottery() public {}
+
+    // function endLottery() public {}
+
+    // function pickWinner() {}
+}
